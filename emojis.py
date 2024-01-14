49133b69f9486b7eab6269b4d09854d22e6e627b
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-01-14 18:37:08 +0200

    get country emoji

---
+++ emojis.py
@@ -0,0 +1,20 @@
+
+
+def get_emoji(pair):
+    if pair == "USD":
+        return "🇺🇸"
+    elif pair == "EUR":
+        return "🇪🇺"
+    elif pair == "GBP":
+        return "🇬🇧"
+    elif pair == "CAD":
+        return "🇨🇦"
+    elif pair == "CNY":
+        return "🇨🇳"
+    elif pair == "JPY":
+        return "🇯🇵"
+    elif pair == "CHF":
+        return "🇨🇭"
+    elif pair == "AUD":
+        return "🇦🇺"
+    
