96a16db2c5ef6287eaa78479776f66c7e556ad4a
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-01-14 18:44:50 +0200

    Leave just the news handler message

--- responses.py
+++ responses.py
@@ -1,23 +1,8 @@
 from random import choice, randint
 from economic_calendar import get_fomarted_news
 
-
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
 
-    if lowered == '':
-        return 'Well, you\'re awfully silent...'
-    elif 'hello' in lowered:
-        return 'Hello there!'
-    elif 'how are you' in lowered:
-        return 'Good, thanks!'
-    elif 'bye' in lowered:
-        return 'See you!'
-    elif 'news' in lowered:
+    if 'news' in lowered:
         return get_fomarted_news()
-    elif 'roll dice' in lowered:
-        return f'You rolled: {randint(1, 6)}'
-    else:
-        return choice(['I do not understand...',
-                       'What are you talking about?',
-                       'Do you mind rephrasing that?'])
